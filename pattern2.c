5c46085acb0fafaa52e332ce875a5ac59501cc48
Sidharth K <143076733+sidharth7352@users.noreply.github.com>
2024-06-22 08:20:25 +0530

    star pattern 2

---
+++ pattern2.c
@@ -0,0 +1,22 @@
+/* pattern2
+
+  *****
+  *****
+  *****
+  *****
+  *****
+*/
+
+#include<stdio.h>
+void main()
+{
+ int i,j;
+ for(i=0;i<5;i++)
+ {
+  for(j=0;j<5;j++)
+  {
+   printf("*");
+   }
+   printf("\n");
+   }
+   }
