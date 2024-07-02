7763b9b9dcee44ab4420151efd424074cbb967d3
Sidharth K <143076733+sidharth7352@users.noreply.github.com>
2024-07-02 21:29:38 +0530

    number pattern

---
+++ pattern7.c
@@ -0,0 +1,23 @@
+/* pattern 7
+
+ 1  3  5  7  9
+11 13 15 17 19
+21 23 25 27 29
+31 33 35 37 39
+41 43 45 47 49
+
+*/
+
+#include<stdio.h>
+void main()
+{
+ int i,j=1,k;
+ for(i=0;i<5;i++)
+ {
+  for(j,k=0;j<5,k<5;j=j+2,k++)
+  {
+   printf("%2d ",j);
+   }
+   printf("\n");
+   }
+   }
