8d5e36f301a79d5aa9bc6131e035c14b3444d7a2
Sidharth K <143076733+sidharth7352@users.noreply.github.com>
2024-07-02 21:29:00 +0530

    number pattern

---
+++ pattern6.c
@@ -0,0 +1,24 @@
+/* pattern 6
+  
+  1  2  3  4  5
+  6  7  8  9 10
+ 11 12 13 14 15
+ 16 17 18 19 20
+ 21 22 23 24 25
+
+*/
+
+#include<stdio.h>
+void main()
+{
+ int i,j=1,k;
+ for(i=0;i<5;i++)
+ {
+  for(j,k=0;j<5,k<5;j++,k++)
+  {
+   printf("%2d ",j);
+   }
+   printf("\n");
+   }
+   }
+   
